371447dc4aa332a1655d3005ee10a83d0667ecb4
David Oberacker <david.oberacker@d4ve.me>
2022-03-03 15:29:43 +0100

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  <div class="flex justify-center m-10">
+    <img alt="Vue logo" src="./assets/logo.png"/>
+  </div>
+  <div>
+    <h1 class="text-center text-3xl font-bold font-italic underline text-blue-600">
+      Hello Vue 3 + TypeScript + Vite
+    </h1>
+    <p class="text-center text-blue-300">
+      This is a test paragraph.
+      I do not know what it does, but we might find out.
+    </p>
+  </div>
+</template>
+
